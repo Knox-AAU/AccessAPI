f9faf9ffb06f8554b603ac18693e0f1e19ac25fd
Frederik <34657773+flja@users.noreply.github.com>
2021-11-24 09:43:33 +0100

    Update SignalRHandler.cs

--- Access API/Access API/SignalR/SignalRHandler.cs
+++ Access API/Access API/SignalR/SignalRHandler.cs
@@ -8,10 +8,39 @@ namespace Access_API.SignalR
 {
     public class SignalRHandler : Hub
     {
+        List<string> connectionId = new List<string>();
             public async Task SendMessage(string user, string message)
             {
                 await Clients.All.SendAsync("ReceiveMessage", user, message);
             }
-        
+
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has joined the group {groupName}.");
+        }
+
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}.");
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            connectionId.Add( Context.ConnectionId);
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            connectionId.Remove(Context.ConnectionId);
+            return base.OnDisconnectedAsync(exception);
+        }
+
+
+
     }
 }
