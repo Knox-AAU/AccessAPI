c1c48e089a7f47816ca81deac63ed034520636ff
Pattrigue <pfas19@student.aau.dk>
2021-12-09 10:48:32 +0100

    RDF controller

---
+++ Access API/Access API/Controllers/RdfController.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Access_API.Controllers
+{
+    [Route("api/wordCount")]
+    [ApiController]
+    [ApiExplorerSettings(GroupName = "Knox WordCount endpoints")]
+    public sealed class RdfController : Controller
+    {
+        [HttpGet]
+        [Route("status")]
+        public IActionResult GetStatus()
+        {
+            HttpWebResponse response = Drivers.HttpRequest.getRequest($"{Urls.RdfUrl}/status");
+
+            return StatusCode((int)response.StatusCode);
+        }
+    }
+}
