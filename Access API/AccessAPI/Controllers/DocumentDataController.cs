6e34ffe1dfe7510ab30e67e01e842d8d09d8223c
KarmaKamikaze <nicolaijgn@gmail.com>
2022-11-08 14:37:28 +0100

    Added summary for swagger docs

--- Access API/AccessAPI/Controllers/DocumentDataController.cs
+++ Access API/AccessAPI/Controllers/DocumentDataController.cs
@@ -8,6 +8,11 @@ namespace Access_API.Controllers
     [ApiExplorerSettings(GroupName = "Knox DocumentDataAPI endpoints")]
     public sealed class DocumentDataController : Controller
     {
+        /// <summary>
+        /// Probes the DocumentDataAPI to check if a connection can be established to the database.
+        /// </summary>
+        /// <response code="200">Success: The database is reachable.</response>
+        /// <response code="500">Internal Server Error: A <see cref="ProblemDetails"/> describing the error.</response>
         [HttpGet]
         [Route("status")]
         public IActionResult GetStatus()
