3cffc024634278a5d0a4ae4b54cddd2139f9a32a
Nikolaj Dam <ndam20@student.aau.dk>
2022-11-25 16:45:20 +0100

    Created Suggester request and response models

---
+++ Access API/AccessAPI/SignalR/SuggesterRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Access_API.SignalR
+{
+    public class SuggesterRequest
+    {
+        public string Sentence { get; set; } = null!;
+        public string OrderBy { get; set; } = null!;
+        public int MaxResults { get; set; }
+    }
+}
---
+++ Access API/AccessAPI/SignalR/SuggesterSentence.cs
@@ -0,0 +1,19 @@
+﻿namespace Access_API.SignalR
+{
+    public class SuggesterSentence
+    {
+        public SuggesterSentence()
+        {
+
+        }
+
+        public SuggesterSentence(string sentence, float score)
+        {
+            Sentence = sentence;
+            Score = score;
+        }
+
+        public string Sentence { get; set; } = null!;
+        public float Score { get; set; }
+    }
+}
