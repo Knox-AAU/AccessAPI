f5ce922acf8dd211922a3d1e7693dfab690a06c2
damniko <71791903+damniko@users.noreply.github.com>
2022-12-01 15:27:12 +0100

    Created Suggester request and response models (#21)

---
+++ Access API/AccessAPI/SignalR/SuggesterResponse.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Access_API.SignalR
+{
+    public class SuggesterResponse
+    {
+        public SuggesterResponse()
+        {
+
+        }
+
+        public SuggesterResponse(List<SuggesterSentence> sentences)
+        {
+            ResultLength = sentences.Count;
+            Results = sentences;
+        }
+
+        public int ResultLength { get; set; }
+        public List<SuggesterSentence> Results { get; set; } = new();
+    }
+}
